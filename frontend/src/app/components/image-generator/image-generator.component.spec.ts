6dab665adc0c6088da9fd1235a6ba9045ba9908f
Manikandan Prabhu <manikandanrprabhu@gmail.com>
2025-05-25 11:21:49 +0530

    Done unit testing configuration.

--- frontend/src/app/components/image-generator/image-generator.component.spec.ts
+++ frontend/src/app/components/image-generator/image-generator.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ImageGeneratorComponent } from './image-generator.component';
+import { GenerateImageService } from 'src/app/services/generate-image.service';
+import { environment } from 'src/environments/environment';
+
+class MockGenerateImageService {}
 
 describe('ImageGeneratorComponent', () => {
   let component: ImageGeneratorComponent;
@@ -8,9 +12,12 @@ describe('ImageGeneratorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ImageGeneratorComponent ]
-    })
-    .compileComponents();
+      declarations: [ImageGeneratorComponent],
+      imports: [environment],
+      providers: [
+        { provide: GenerateImageService, useClass: MockGenerateImageService },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ImageGeneratorComponent);
     component = fixture.componentInstance;
--- frontend/src/app/services/generate-image.service.spec.ts
+++ frontend/src/app/services/generate-image.service.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GenerateImageService } from './generate-image.service';
+import { environment } from 'src/environments/environment';
 
 describe('GenerateImageService', () => {
   let service: GenerateImageService;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [environment],
+    });
     TestBed.configureTestingModule({});
     service = TestBed.inject(GenerateImageService);
   });
